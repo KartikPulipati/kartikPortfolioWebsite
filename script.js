c662a8552a2bec3269d585c59a4bb7e624e23044
Kartik M.P <Kartik.pulipati@gmail.com>
2023-07-23 12:50:40 -0700

    V2.2.0
    Added a better stationary navbar and cleaned up javascript code that handles transformation of navbar. Also, experimenting with adding opacity to mobile navbar.

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      document.getElementById("myNavbar").style.marginLeft = "50px";
-      document.getElementById("myNavbar").style.marginRight = "50px";
-      document.getElementById("myNavbar").style.marginTop = "5px";
-      document.getElementById("myNavbar").style.background = "rgba(255, 255, 255, 1)";
-      document.getElementById("myNavbar").style.borderRadius = "50px";
+      document.getElementById("myNavbar").classList.remove("stationary");
+      document.getElementById("myNavbar").classList.add("mobile");
   
     } else {
-      document.getElementById("myNavbar").style.marginLeft = "20px";
-      document.getElementById("myNavbar").style.marginRight = "20px";
-      document.getElementById("myNavbar").style.marginTop = "20px";
-      document.getElementById("myNavbar").style.background = "transparent";
-      document.getElementById("myNavbar").style.borderRadius = "10px";
+      document.getElementById("myNavbar").classList.remove("mobile");
+      document.getElementById("myNavbar").classList.add("stationary");
     }
   }
   
