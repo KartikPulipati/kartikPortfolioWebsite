dd47a8cd769ac9a5e59ebd29c7ba57de36e5ba18
Kartik M.P <Kartik.pulipati@gmail.com>
2023-08-05 17:57:13 -0700

    V3.1.0
    added a background to the navbar when the navbar is uncollapsed(for smaller screens) so the navbar's contents can actually be viewed.

--- js/navbar.js
+++ js/navbar.js
@@ -2,13 +2,23 @@ function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("myNavbar").classList.remove("stationary");
     document.getElementById("myNavbar").classList.add("mobile");
-
-
   } else {
     document.getElementById("myNavbar").classList.remove("mobile");
     document.getElementById("myNavbar").classList.add("stationary");
   }
 }
 
+function toggleMenu() {
+  let hamburger = document.getElementsByClassName("navbar-toggler");
+  let menu = document.getElementById("myNavbar");
+  if (!hamburger[0].classList.contains("collapsed") && menu.classList.contains("stationary")) {
+    document.getElementById("myNavbar").classList.add("opened-navbar");
+    
+  }else{
+    document.getElementById("myNavbar").classList.remove("opened-navbar");
+  }
+}
 window.onscroll = function () { scrollFunction() };
+document.getElementsByClassName("navbar-toggler")[0].onclick = function () { toggleMenu() };
+
 
